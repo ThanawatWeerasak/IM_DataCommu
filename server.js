40ca055b5edb211fed5404dd50d36911e7810c1f
ThanawatWeerasak <thanawattw.work@gmail.com>
2025-09-26 13:59:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
@@ -21,4 +22,5 @@ io.on("connection", function (socket) {
   });
 });
 
-server.listen(5000);
+
+server.listen(PORT);

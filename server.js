e4280e73e1daf1e60cc7cdfae3aea68d87eda1dc
ThanawatWeerasak <thanawattw.work@gmail.com>
2025-10-01 21:59:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs").promises
 const PORT = process.env.PORT || 5000;
 const app = express();
 const server = require("http").createServer(app);
@@ -20,8 +20,25 @@ io.on("connection", function (socket) {
   socket.on("chat", function (message) {
     socket.broadcast.emit("chat", message);
   });
+
+  socket.on("image", async (image) => {
+    const { username, base64 } = image;
+    const buffer = Buffer.from(base64, "base64");
+
+    const dir = path.join(__dirname, "tmp");
+    const filename = path.join(dir, `image-${Date.now()}-${username}.png`);
+
+    try {
+      await fs.mkdir(dir, { recursive: true }); // ensures folder exists
+      await fs.writeFile(filename, buffer);
+      socket.broadcast.emit("image", { username, base64 });
+    } catch (err) {
+      console.error("Error saving image:", err);
+    }
+  });
 });
 
 
 server.listen(PORT);
+
 
